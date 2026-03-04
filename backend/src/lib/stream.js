31b6106fe31632fa67c093debd8322611e402105
Arpit Kumar <iamarpitgoyal069@gmail.com>
2026-03-04 23:55:41 +0530

    steam-auth-sync

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,32 +1,29 @@
 import { StreamChat } from "stream-chat"
 import { ENV } from "./env.js"
 
-
 const apiKey = ENV.STREAM_API_KEY
 const apiSecret = ENV.STREAM_API_SECRET
 
 if (!apiKey || !apiSecret) {
-    console.log("STREAM_API_KEY or STREAM_API_SECRET is missing");
+    console.log("STREAM_API_KEY or STREAM_API_SECRET is missing")
 }
 
-export const chatClient = new StreamChat(apiKey, apiSecret)
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret)
 
 export const upsertStreamUser = async (userData) => {
     try {
         await chatClient.upsertUser(userData)
         console.log("Stream user upserted successfully:", userData)
-    }
-    catch (err) {
+    } catch (err) {
         console.error("Error upserting Stream user:", err)
     }
 }
 
 export const deleteStreamUser = async (userId) => {
     try {
-        await chatClient.deleteUser(userId)
-        console.log("Stream User deleted Successfully:", userId)
-    }
-    catch (err) {
+        await chatClient.deleteUser(userId, { hard_delete: true })
+        console.log("Stream user deleted successfully:", userId)
+    } catch (err) {
         console.error("Error deleting Stream user:", err)
     }
-} 
+}
