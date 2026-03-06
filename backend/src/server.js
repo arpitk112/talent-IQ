b857b70193c130d6754b7d32c6ff8523b5a7f2a1
Arpit Kumar <iamarpitgoyal069@gmail.com>
2026-03-06 22:12:59 +0530

    session routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,9 @@ import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import { inngest, functions } from "./lib/inngest.js";
 import { clerkMiddleware } from '@clerk/express'
+
 import chatRoutes from "./routes/chatRoutes.js"
+import sessionRoutes from "./routes/sessionRoutes.js"
 
 const app = express();
 
@@ -25,6 +27,7 @@ if (ENV.NODE_ENV !== "production") {
 
 app.use("/api/inngest", serve({ client: inngest, functions }))
 app.use("/api/chat", chatRoutes)
+app.use("/api/sessions", sessionRoutes)
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "api is up and running" })
