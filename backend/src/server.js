386e6839b6d2dad0b0dfa44500aaea28d4e29db6
Arpit Kumar <iamarpitgoyal069@gmail.com>
2026-03-16 01:25:45 +0530

    render deploy config

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,8 +42,8 @@ app.get("/health", (req, res) => {
 if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
